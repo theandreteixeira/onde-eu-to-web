8cfd88c207531c0a8b218ab19b4e843058df2fbd
Andre Teixeira <andreteixeira628@gmail.com>
2025-09-08 09:47:24 -0300

    feat: add import for institucional route

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout.jsx';
 import DeleteAccount from './pages/Exclusao.jsx';
 import Home from './pages/Home.jsx';
+import Institutional from './pages/Institutional.jsx';
 import Privacy from './pages/Privacy.jsx';
 import Terms from './pages/Terms.jsx';
 
@@ -13,7 +14,7 @@ export default function RoutesView() {
         <Route path="/termos" element={<Terms />} />
         <Route path="/privacidade" element={<Privacy />} />
         <Route path="/exclusao" element={<DeleteAccount />} />
-         <Route path="/institucional" element={<Institutional />} />
+        <Route path="/institucional" element={<Institutional />} />
       </Routes>
     </Layout>
   );
